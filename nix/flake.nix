959c2e98b7893235c653533d4bea7db6f1f05f74
davethai <dave@davethai.com>
2024-12-10 19:20:36 -0500

    break up configuration

--- nix/flake.nix
+++ nix/flake.nix
@@ -10,14 +10,39 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
   let
-    configuration = { pkgs, config, ... }: {
-
+    coreConfiguraton = { pkgs, config, ... }: {
       nixpkgs.config.allowUnfree = true;
+            
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
 
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;
+
+      # Enable direnv
+      programs.direnv.enable = true;
+    };
+
+    packageConfiguration = { pkgs, config, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
-      environment.systemPackages = with pkgs;
-      [
+      environment.systemPackages = with pkgs; [
           # Mac Requirement
           mkalias
           # Terminal emulator
@@ -29,7 +54,16 @@
           git
           stow
         ];
-      
+
+      fonts.packages = with pkgs; [
+        nerd-fonts.fira-code
+        nerd-fonts.droid-sans-mono
+
+        nerd-fonts.symbols-only  #This one
+      ];
+    };
+
+    homebrewConfiguration = { pkgs, config, ... }: {
       homebrew = {
         enable = true;
         brews = [
@@ -79,35 +113,9 @@
         onActivation.autoUpdate = true;
         onActivation.upgrade = true;
       };
+    };
 
-      fonts.packages = with pkgs; [
-        nerd-fonts.fira-code
-        nerd-fonts.droid-sans-mono
-
-        nerd-fonts.symbols-only  #This one
-      ];
-
-      # Mac Alias - Nix GUI apps show in Spotlight search
-      system.activationScripts.applications.text = let
-        env = pkgs.buildEnv {
-          name = "system-applications";
-          paths = config.environment.systemPackages;
-          pathsToLink = "/Applications";
-        };
-      in
-        pkgs.lib.mkForce ''
-        # Set up applications.
-        echo "setting up /Applications..." >&2
-        rm -rf /Applications/Nix\ Apps
-        mkdir -p /Applications/Nix\ Apps
-        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-        while read -r src; do
-          app_name=$(basename "$src")
-          echo "copying $src" >&2
-          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-        done
-            '';
-      
+    macDefaults = { pkgs, config, ... }: {
       system.defaults = {
         NSGlobalDomain.AppleICUForce24HourTime = true;
         NSGlobalDomain.AppleInterfaceStyle = "Dark";
@@ -133,31 +141,29 @@
           BatteryShowPercentage = true;
         };
       };
-      
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+    };
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;
-
-      # Enable direnv
-      programs.direnv.enable = true;
+    aliasConfiguration = { pkgs, config, ... }: {
+      # Mac Alias - Nix GUI apps show in Spotlight search
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+        # Set up applications.
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read -r src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        done
+            '';
     };
   in
   {
@@ -165,7 +171,11 @@
     # $ darwin-rebuild build --flake .#Daves-MacBook-Pro
     darwinConfigurations."Daves-MacBook-Pro" = nix-darwin.lib.darwinSystem {
       modules = [ 
-        configuration
+        coreConfiguraton
+        packageConfiguration
+        homebrewConfiguration
+        macDefaults
+        aliasConfiguration
         nix-homebrew.darwinModules.nix-homebrew
         {
           nix-homebrew = {
