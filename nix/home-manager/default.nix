72f6d9a819617c3597963ea7bd9941f34516f334
Dave Thai <152396315+davethai@users.noreply.github.com>
2024-12-30 04:59:00 -0500

    Update default.nix

--- nix/home-manager/default.nix
+++ nix/home-manager/default.nix
@@ -1,8 +1,8 @@
-{user, ...}: {
+{user, lib, ...}: {
     home-manager = {
     verbose = true;
     useGlobalPkgs = true;
     useUserPackages = true;
-    users.${user} = ./${user}/home.nix;
+    users.${user} = lib.attrsets.genAttrs ./${user}/home.nix;
   };
 }
