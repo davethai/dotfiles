eb8035dfc3d41614514b5c32ca44527abe6f8713
davethai <dave@davethai.com>
2025-01-04 20:24:37 -0500

    wsl user

--- nix/modules/wsl/default.nix
+++ nix/modules/wsl/default.nix
@@ -1,4 +1,8 @@
 { ... }: {
+  imports = [
+    ./users
+  ];
+
   system.stateVersion = "24.05";
   wsl.enable = true;
 }
---
+++ nix/modules/wsl/users/default.nix
@@ -0,0 +1,5 @@
+{ ... }: {
+  users.users.davethai.isSystemUser = true;
+  users.users.davethai.group = "davethai";
+  users.groups.davethai = {};
+}
