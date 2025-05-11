a05790c58801ad49414059e5ac3acf12cb8ebbcd
davethai <dave@davethai.com>
2025-05-11 09:23:28 -0400

    k8s get alias

--- nix/homes/davethai/programs/terminal/shell/zsh/aliases.nix
+++ nix/homes/davethai/programs/terminal/shell/zsh/aliases.nix
@@ -19,7 +19,30 @@ in {
 
     # k8s
     k = "kubectl";
+    # Get
+    kga = "kubectl get all";
+    kgans = "kubectl get all --all-namespaces";
     kgp = "kubectl get pods";
+    kgsvc = "kubectl get services";
+    kgep = "kubectl get endpoints";
+    kgns = "kubectl get namespaces";
+    kgno = "kubectl get nodes";
+    kgpv = "kubectl get persistentvolumes";
+    kgpvc = "kubectl get persistentvolumeclaims";
+    kgcm = "kubectl get configmaps";
+    kgs = "kubectl get secrets";
+    kgs = "kubectl get secrets";
+    kgev = "kubectl get events";
+    kgsa = "kubectl get serviceaccounts";
+    kgd = "kubectl get deploy";
+    kgrs = "kubectl get replicasets";
+    kgsts = "kubectl get statefulsets";
+    kgds = "kubectl get daemonsets";
+    kgj = "kubectl get jobs";
+    kgcj = "kubectl get cronjobs";
+    kging = "kubectl get ingresses";
+    kging = "kubectl get ingresses";
+    kgnetpol = "kubectl get networkpolicies";
 
     # quick navigation
     ".." = "cd ..";
